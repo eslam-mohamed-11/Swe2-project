9a80d274c01f6d72048547e021e41c1857b1591f
eslam-mohamed-11 <eslamahly659@gmail.com>
2024-05-14 09:30:23 +0300

    remove debug

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'chat app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
